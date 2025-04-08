7026e499433d9b35303a716164a82abeb78ece04
Tugba Karasu <tugbakarasu@tamu.edu>
2025-04-08 13:59:34 -0500

    Add files via upload

---
+++ GNUmakefile
@@ -0,0 +1,12 @@
+DIM = 2
+
+TINY_PROFILE = FALSE
+
+DEBUG = FALSE
+
+USE_MPI = TRUE
+USE_OMP = FALSE
+
+include ./Make.package
+include ../Make.CNS
+
---
+++ cns_prob_parm.cpp
@@ -0,0 +1,14 @@
+#include "cns_prob_parm.H"
+#include "CNS.H"
+#include "CNS_index_macros.H"
+
+#include <AMReX_Arena.H>
+
+ProbParm::ProbParm ()
+{
+}
+
+ProbParm::~ProbParm ()
+{
+    
+}
